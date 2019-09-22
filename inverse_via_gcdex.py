8e2c3beea4e45d85b279da36addff6f429d35707
dima17502 <53102183+dima17502@users.noreply.github.com>
2019-09-22 19:02:30 +0300

    Update inverse_via_gcdex.py

--- inverse_via_gcdex.py
+++ inverse_via_gcdex.py
@@ -29,5 +29,34 @@ m, n = map(int, input().split())
 print(inv_elem(m, n))
 
 
+def gcdex(a, b):
+    if a >= b >= 0 and a + b > 0:
+        if b == 0:
+            d, x, y = (a, 1, 0)
+        else:
+            d, p, q = gcdex(b, a % b)
+            x = q
+            y = p - (a // b) * q
+        return d, x, y
+    elif  b > a > 0:
+        d, x, y = gcdex(b, a)
+        return d, y, x
+    else:
+        print("Invalid input")
+        return -1
+
+
+def inv_elem(a, b):
+    d, x, y = gcdex(a, b)
+    if gcdex(a, b)[0] == 1:
+        print("great common divisor and inverse element are:")
+        return d, x
+    print("Inverse element doesn't exist")
+    return -1
+
+
+m, n = map(int, input().split())
+
+print(*inv_elem(m, n))
 
 
